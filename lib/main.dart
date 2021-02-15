4792fd40c080cdf208f3324a968ab583eef9df79
Devfx <devfx@Devfxs-MacBook-Pro.local>
2021-02-15 11:15:21 +0700

    Update Belajar

--- lib/main.dart
+++ lib/main.dart
@@ -5,50 +5,50 @@ void main() {
   runApp(MyApp());
 }
 
-// buat class my app
-// Stateful widget memiliki 2 class
-// class 1 yaitu my app dan yang ke 2 class state dan kita melakukan isi di class ke 2
 class MyApp extends StatefulWidget {
   @override
   _MyAppState createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-  // Buat method tekan tombol
-  // method void untuk tidak mengembalikan apa-apa
+  // membuat methdo untuk menampilkan di text dengan string
+  String message = "Ini adalah Text";
 
-  int number = 0;
-  void tekanTombol() {
-    // untuk me refresh tampilkan dengan set state atau fungsi untuk merubah tampilan untuk state saat ini
-    // Mengubah state saat di tambah
-    setState(() {
-      // untuk menambah bilangan
-      number = number + 1;
-    });
-  }
+  // Membuat void untuk di tekan di onpress
+
+// Hapus method void untuk membuat message di onpress dengan method anonymous
+  // void tombolDitekan() {
+  //   // Memanpilkan pesan
+  //   // Buat setState biar tampilak terupdate
+  //   setState(() {
+  //     message = "Tombol sudah di tekan";
+  //   });
+  // }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Belajar Stateful Widget"),
+          title: Text("Anonymous Method"),
         ),
         body: Center(
+          // dalam widget center ada widget column
           child: Column(
-            // Namabh mainaxis untuk pidanh ke tengah
             mainAxisAlignment: MainAxisAlignment.center,
+            // Membuat 2 widget dalam childreen
             children: <Widget>[
-              // membuat text semakin besar saat state di tambah
-              Text(
-                number.toString(),
-                style: TextStyle(fontSize: 10 + number.toDouble()),
-              ),
+              // Panggil method message
+              Text(message),
               RaisedButton(
-                child: Text("Tambah"),
-                // buat method untuk di panggil ke dalam onpress
-                // misalkan method tekanTombol
-                onPressed: tekanTombol,
+                child: Text("Tekan saya"),
+                // Anonymous metode
+                // Untuk menampilkan message
+                onPressed: () {
+                  setState(() {
+                    message = "Tombol sudah di tekan";
+                  });
+                },
               )
             ],
           ),
@@ -58,7 +58,5 @@ class _MyAppState extends State<MyApp> {
   }
 }
 
-// Widget dibagi menjadi 2 yaitu
-// Stateless itu adalah widget yang tidak memiliki state atau tidak keadaan
-// perumpamaan stateless logo dan title appar tidak pernah berubah
-// statefull widget yaitu widget sesuai dengan keadaannya saat button di klik bisa berubah-ubah
+// Kapan kita menggunakan anonymous method ? kita hanya memanggil satu kali saja
+// tidak memanggil di tempat lain atau meringkas code program lebih ringkas lagi
